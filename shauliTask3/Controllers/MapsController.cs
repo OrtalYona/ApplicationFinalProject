edd9f53d3aab16caa8f10fedfdac5a30daff96c8
OrtalYona <ortalyona94@gmail.com>
2017-10-02 19:04:28 +0300

    updates

---
+++ shauliTask3/Controllers/MapsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using shauliTask3.Models;
+
+namespace shauliTask3.Controllers
+{
+    public class MapsController : Controller
+    {
+        private MapsDbContext db = new MapsDbContext();
+
+        // GET: Maps
+        public ActionResult Index()
+        {
+            return View(db.Map.ToList());
+        }
+
+        // GET: Maps/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Maps maps = db.Map.Find(id);
+            if (maps == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maps);
+        }
+
+        // GET: Maps/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Maps/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MapsID,Latitude,Longitude")] Maps maps)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Map.Add(maps);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(maps);
+        }
+
+        // GET: Maps/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Maps maps = db.Map.Find(id);
+            if (maps == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maps);
+        }
+
+        // POST: Maps/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MapsID,Latitude,Longitude")] Maps maps)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(maps).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(maps);
+        }
+
+        // GET: Maps/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Maps maps = db.Map.Find(id);
+            if (maps == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maps);
+        }
+
+        // POST: Maps/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Maps maps = db.Map.Find(id);
+            db.Map.Remove(maps);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- shauliTask3/Controllers/PostsController.cs
+++ shauliTask3/Controllers/PostsController.cs
@@ -14,6 +14,8 @@ namespace shauliTask3.Controllers
     public class PostsController : Controller
     {
         private PostContext db = new PostContext();
+        private MapsDbContext maps = new MapsDbContext();
+
 
 
         // GET: Posts
@@ -69,7 +71,49 @@ namespace shauliTask3.Controllers
 
 
         public ActionResult Home()
-        { 
+        {
+            
+          //  Maps mofo = null;
+            List<Maps> mofo = new List<Maps>();
+            foreach (var m in maps.Map)
+            {
+                mofo.Add(m);
+                //  break;
+            }
+
+            if (mofo != null)
+            {
+                foreach (var m in maps.Map)
+                {
+                    
+                    ViewBag.Latitude = mofo.First().Latitude;
+                    ViewBag.Longtitude = mofo.First().Longitude;
+                    //  ViewBag.Latitude = mofo.Latitude;
+                    //   ViewBag.Longtitude = mofo.Longitude;
+                }
+
+            }
+            else
+            {
+                ViewBag.Latitude = 51.122;
+                ViewBag.Longtitude = 0;
+            }
+            using (PostContext db=new PostContext())
+            {
+                ViewBag.TotalPosts = db.Posts.Count();
+                ViewBag.TotalComments = db.comments.Count();
+            }
+            using (AccountDbContext db1 = new AccountDbContext())
+            {
+                ViewBag.TotalAccounts = db1.userAccounts.Count();
+
+            }
+            using (FanDBContext db2 = new FanDBContext())
+            {
+                ViewBag.TotalFans = db2.Fan.Count();
+
+            }
+
             return View(db.Posts.ToList());
         }
         // GET: Posts/Details/5
@@ -196,15 +240,36 @@ namespace shauliTask3.Controllers
         //here we need the group by
         public ActionResult Statistics()
         {
+           // List<Post> posts = db.Posts.ToList();
+         //   List<Comment> comments = db.comments.ToList();
+
 
             var query = from i in db.Posts
                         group i by i.postWriter into g
                         select new { PostWriter = g.Key, c=g.Count() };
+            return View(query.ToList());
+
+            //List<Post> _postList = db.Post.ToList();
+            //List<Comment> _commentList = db.Comment.ToList();
+
+            //var itemOrders =
+            //    from p in _postList
+            //    join c in _commentList on p.PostID equals c.PostID
+            //    select new { p.subjectPost, c.subjectComment };
+
+            //return View(itemOrders.ToList());
+
+
+        }
+        public ActionResult Join()
+        {
+
+            var query = from d in db.Posts
+                         join j in db.comments on d.PostID equals j.PostID
+                         select new { d.PostTitle, j.CommentTitle };
+
 
             return View(query.ToList());
-      
-
-
         }
 
         protected override void Dispose(bool disposing)
